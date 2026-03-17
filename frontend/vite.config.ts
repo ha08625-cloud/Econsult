969690e4e34b519d6c1468335f105a95f456f4d5
ha08625-cloud <ha08625@googlemail.com>
2026-03-17 09:51:18 +0000

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-/// <reference types="vitest" />
 
 export default defineConfig({
   plugins: [react()],
