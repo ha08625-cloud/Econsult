ab90ad70d94f65e92ed1c51686e54f61297a32e7
ha08625-cloud <ha08625@googlemail.com>
2026-03-31 11:42:25 +0100

    added on back to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -464,6 +464,7 @@ export default function App() {
           setPresentationFetchTrigger((k) => k + 1);
           setScreen("FREE_TEXT");
         }}
+        onBack={() => setScreen("OUTCOME")}
       />
     );
   }
